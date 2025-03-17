554942a5b5a87ed38d12938f62c5b46064c3c156
miladatasys <camilamorales.it@gmail.com>
2025-03-17 20:30:12 -0300

    pye

--- py4e/loops_iterations.py
+++ py4e/loops_iterations.py
@@ -5,8 +5,14 @@ while n > 0 :
 print('Blastoff!')
 print(n)
 
-n = 5
-while n > 5 :
-    print('Lather')
-    print('Rinse')
-print('Dry off!')
+for i in [5,4,3,2,1] :
+    print(i)
+print('Blastoff!')
+
+largest_so_far = -1
+print('Before', largest_so_far)
+for the_num in [9,41,12,3,74,15]:
+    if the_num > largest_so_far:
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+print('After', largest_so_far)
